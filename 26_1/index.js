4a1986090bd7c1c92828fc617890315d8193fa1f
vuthedieu <vtdieu02@gmail.com>
2024-02-21 20:10:30 +0700

    22-1

---
+++ 26_1/index.js
@@ -0,0 +1,4 @@
+alert("xin chao moi nguoi");
+document.write("xin chao moi nguoi");
+document.write("<h1>xin chao</h1>");
+console.log("xin chao");
---
+++ main.js
@@ -0,0 +1,34 @@
+// alert('hi js basic')
+// var fullName='vu the dieu';
+// var age ='21';
+// alert(fullName)// hiển thị thông báo
+// alert(age)
+
+//console
+// console.log('day la console log')
+// console.log(fullName)
+
+
+
+//confirm
+// confirm('xác nhận thoát')
+
+// prompt('dien tuoi')
+
+
+
+// setTimeout(function()  {
+
+//     alert('thong bao')
+// }, 100);
+
+
+ 
+
+var a=1+2;
+console.log(a)
+
+
+
+
+
